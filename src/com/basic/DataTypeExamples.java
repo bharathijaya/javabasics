b82e129158b0d0454d2f993b59c5e7ec09180a48
bharathijaya <jayabharathi9897@gmail.com>
2018-05-29 03:50:38 -0700

    Operators

--- src/com/basic/DataTypeExamples.java
+++ src/com/basic/DataTypeExamples.java
@@ -4,10 +4,14 @@ public class DataTypeExamples {
  public static void main(String[] args)
  {
 	 int count = 25;
-	 if(count > 20)
-		 System.out.println(25);
-	 else
+	 if(count > 20) {
 		 System.out.println(20);
- }
-
-}
+	 }
+	 else
+	 
+		 System.out.println(25);
+	
+ 
+	int back  = count < 30 ? 20 : 45;
+ }	
+	 }
---
+++ src/com/basic/Operators.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class Operators {
+	public static void main(String[]args)
+	{
+		int choice = 100;
+		switch(choice)
+		{
+		case 10:
+			System.out.println(10);
+			break;
+		case 30:
+			System.out.println(30);
+			break;
+		case 100:
+			System.out.println(100);
+			break;
+			default:
+				System.out.println("default");
+				break;
+		}
+			
+	}
+  
+}
