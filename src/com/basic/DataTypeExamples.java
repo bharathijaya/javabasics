13614cec0c1a2085cc1563c82b9b5eb0b7a02565
bharathijaya <jayabharathi9897@gmail.com>
2018-05-29 03:16:08 -0700

    datatype examples

---
+++ src/com/basic/DataTypeExamples.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class DataTypeExamples {
+ public static void main(String[] args)
+ {
+	 int count = 25;
+	 if(count > 20)
+		 System.out.println(25);
+	 else
+		 System.out.println(20);
+ }
+
+}
