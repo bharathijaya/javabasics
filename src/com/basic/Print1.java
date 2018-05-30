e7cad36907fffcc2eb6de9edd6f3f9fd15637505
bharathijaya <jayabharathi9897@gmail.com>
2018-05-29 22:56:30 -0700

    Print1

---
+++ src/com/basic/Print1.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class Print1 {
+	public static void main( String[] args )
+	{
+	for ( int i = 1; i <= 10; i++ )
+	{
+	for ( int j = 1; j <= 5; j++ )
+	System.out.print( '@' );
+
+	 System.out.println();
+	 }
+	}
+	 }
+
+
+
---
+++ src/com/basic/Total.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class Total {
+	public static void main(String[]args)
+	{
+		int x=1;
+		while ( x <= 10 )
+		{
+		//total += x;
+		++x;
+		}
+
+
+	}
+
+}
