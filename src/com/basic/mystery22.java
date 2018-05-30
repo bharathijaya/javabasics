3f8fe838776e96c3c3fbad8a9d4ba917233af52c
bharathijaya <jayabharathi9897@gmail.com>
2018-05-30 01:42:57 -0700

    mystery22

---
+++ src/com/basic/mystery22.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class mystery22 {
+	public static void main( String[] args )
+{
+int count = 1;
+
+while ( count <= 10 )
+{
+ System.out.println( ((count % 2) == 1) ? "****" : "++++++++" );
+ ++count;
+ } 
+ } 
+ } 
