ea492d0a77729fd7770e906ef1dac4e2f95c92cb
bharathijaya <jayabharathi9897@gmail.com>
2018-05-29 21:44:16 -0700

    age

---
+++ src/com/basic/Age.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Age {
+	private static int age;
+
+	public static void main(String[]args)
+	{
+ if ( age >= 65 )
+System.out.println( "age is greater than or equal to 65" );
+else
+System.out.println( "age is less than 65 ");
+
+	}
+}
