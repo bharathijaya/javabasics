afe9fb2f03e631a8ca91e3392f67c690e1bf94a3
bharathijaya <jayabharathi9897@gmail.com>
2018-05-30 23:54:29 -0700

    Invoice

---
+++ src/com/basic/Invoice.java
@@ -0,0 +1,62 @@
+package com.basic;
+
+public class Invoice {
+	String number;
+	String description;
+	int quantity;
+	double price;
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		if(quantity<0) {
+			this.quantity = 0;
+		}
+		else
+		{
+			this.quantity = quantity;
+		}
+		
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		if(price < 0.0) {
+			this.price = 0.0;
+		}
+		else
+		{
+			this.price = price;
+		}
+		
+	}
+	public double getInvoiceAmount()
+	{
+		 return  price*quantity;
+		
+	}
+	public static void main(String[] args)
+	{
+		Invoice v1 = new Invoice();
+		v1.setQuantity(10);
+		v1.setPrice(100);
+		v1.setDescription("bolt");
+		System.out.println(v1.getQuantity());
+		System.out.println(v1.getInvoiceAmount());
+		System.out.println(v1.getPrice());
+	}
+
+}
