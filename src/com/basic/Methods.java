66a46e19e995a3ac5f7bd60f3d593dec68884fcc
bharathijaya <jayabharathi9897@gmail.com>
2018-05-31 05:37:04 -0700

    Multiple

---
+++ src/com/basic/Methods.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class Methods {
+	public boolean isEven(int one) {
+		return one % 2 == 0 ? true : false;
+	}
+ public static void main(String[] args) {
+	 Methods m1 = new Methods();
+	 m1.
+ }
+}
--- src/com/basic/Multiple.java
+++ src/com/basic/Multiple.java
@@ -1,6 +1,17 @@
 package com.basic;
 
 public class Multiple {
-	public static void main(String[] args)
+	public static void main(String[]args) {
+		int num=2;
+		if(num % 2 == 0)
+		{
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+		
+	}
 
 }
