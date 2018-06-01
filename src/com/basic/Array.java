a018c3091c2a99b3e294d9954532d562e6658d4f
bharathijaya <jayabharathi9897@gmail.com>
2018-06-01 03:35:59 -0700

    Array

---
+++ src/com/basic/Array.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+public class Array {
+	public static void main(String[]args) {
+int n=1234;
+int i=0;
+int t;
+int arr[] = new int[10];
+while(n>0)
+{
+	t = n%10;
+	arr[i]=t;
+	i++;
+	
+	//sum = (sum * 10 )+ t;
+	n = n/10;
+	//System.out.println(t);
+}
+for( i=0;i<10;i++) {
+	System.out.println(arr[i]);
+ }
+
+}
+}
