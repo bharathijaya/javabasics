5ffc142f412ee7ca7ba58763bbc40ea91e6f1b8c
bharathijaya <jayabharathi9897@gmail.com>
2018-06-04 21:34:11 -0700

    ControlProgram

---
+++ src/com/basic/Test.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class Test {
+	public static void main(String[]args) {
+		int arr1[] = {1,2,3};
+		int arr2[] = {1,2,3};
+		if(arr1 == arr2) {
+			System.out.println("same");
+		}
+		else {
+			
+			System.out.println(" not same");
+		}
+	}
+
+}
---
+++ src/com/basic/Value.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class Value {
+	public static void main(String[]args) {
+		int arr[] = new int[7];
+		int max = getMax(arr);
+		in
+	}
+
+}
